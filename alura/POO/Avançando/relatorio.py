72f359578598be2899ebd69d34e0d669ccf0d959
lobhaum <contato@eduardoluciano.com.br>
2019-12-16 21:06:37 -0300

    add file relatorio

---
+++ alura/POO/Avançando/relatorio.py
@@ -0,0 +1,23 @@
+class Relatorio:
+    def gera_relatorio(self):
+        print('Relatorio geral')
+
+
+class RelatorioUsuarios(Relatorio):  # extendendo classe Relatorio
+    def gera_relatorio(self):
+        print('Relatorio dos usuarios')
+
+
+class RelatorioCustos(Relatorio):  # extendendo classe Relatorio
+    def gera_relatorio(self):
+        print('Relatorio de custos')
+
+
+relatorio1 = RelatorioUsuarios()
+relatorio2 = RelatorioCustos()
+relatorio3 = RelatorioUsuarios()
+relatorio4 = RelatorioUsuarios()
+
+relatorios = [relatorio1, relatorio2, relatorio3, relatorio4]
+for rel in relatorios:
+    rel.gera_relatorio()  # chama metodo polimorfista gera_relatorio
