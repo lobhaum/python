5e697125d4ebddc4e8eccbf60c13f7d72bf58c70
lobhaum <contato@eduardoluciano.com.br>
2019-12-17 13:18:07 -0300

    addfiles

--- alura/Flask/jogoteca/jogoteca.py
+++ alura/Flask/jogoteca/jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -8,16 +8,42 @@ class Jogo:
         self.nome = nome
         self.categoria = categoria
         self.console = console
+        
+jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
+jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
+jogo3 = Jogo('World of Warcraft', 'MMORPG', 'PC')
+lista = [jogo1, jogo2, jogo3]
 
-
-@app.route('/inicio')
-def ola():
-    jogo1 = Jogo('Super Mario', 'Ação','SNES')
-    jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
-    jogo3 = Jogo('World of Warcraft', 'MMORPG', 'PC')
-    lista = [jogo1, jogo2, jogo3]
+@app.route('/')
+def index():
+    
     return render_template('lista.html', titulo='Jogos', 
                            jogos=lista)
 
 
-app.run()
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return redirect('/')   
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if 'mestra' == request.form['senha']:
+        return redirect('/')
+    else:
+        return redirect('/login')
+
+app.run(debug=True)
