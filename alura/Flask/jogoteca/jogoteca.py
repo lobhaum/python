8d26b00ded5b96b60db6091cf1826ca148c26d11
lobhaum <contato@eduardoluciano.com.br>
2019-12-17 09:51:21 -0300

    add files

---
+++ alura/Flask/jogoteca/jogoteca.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/inicio')
+def ola():
+    lista = ['Tetris', 'Super Mario', 'Pokemon Gold', 'World of Warcraft']
+    return render_template('lista.html', titulo='Jogos', 
+                           jogos=lista)
+
+
+app.run()
