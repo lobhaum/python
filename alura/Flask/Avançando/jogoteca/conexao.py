ab94d2fbcb6b10a337b7c370010fc355bb829f35
lobhaum <contato@eduardoluciano.com.br>
2019-12-18 08:44:38 -0300

    Inicio do Avançando em Flask: Criação do banco de dados; Criação das tabelas; Persistindo primeiros dados.

---
+++ alura/Flask/Avançando/jogoteca/conexao.py
@@ -0,0 +1,14 @@
+import MySQLdb
+print('Conectando...')
+conn = MySQLdb.connect(user='programador', passwd='Brasil2020', host='192.168.1.100', port=3306)
+
+cursor = conn.cursor()
+
+cursor.execute('SELECT * from jogoteca.jogo')
+for user in cursor.fetchall():
+    print(f' Jogo: {user[1]} Categoria: {user[2]} Console: {user[3]}')
+
+
+# commitando senão nada tem efeito
+conn.commit()
+cursor.close()
