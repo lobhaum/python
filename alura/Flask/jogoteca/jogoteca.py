1dd338397d341ab2fd9cd59ee004fba011ed6da0
lobhaum <contato@eduardoluciano.com.br>
2019-12-18 01:22:16 -0300

    finalizando Flask 1

--- alura/Flask/jogoteca/jogoteca.py
+++ alura/Flask/jogoteca/jogoteca.py
@@ -10,7 +10,20 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
+class Usuario:
+    def __init__(self, id, nome, senha):
+        self.id = id
+        self.nome = nome
+        self.senha = senha
         
+
+usuario1 = Usuario('Luan', 'Luan Marques', '1234')
+usuario2 = Usuario('Nico', 'Nico Steppat', '7a1')
+usuario3 = Usuario('Flávio', 'Flávio', 'javascript')
+
+usuarios = {usuario1.id: usuario1, 
+            usuario2.id: usuario2, 
+            usuario3.id: usuario3}
 jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
 jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
 jogo3 = Jogo('World of Warcraft', 'MMORPG', 'PC')
@@ -50,10 +63,12 @@ def login():
 
 @app.route('/autenticar', methods=['POST', ])
 def autenticar():
-    if 'mestra' == request.form['senha']:
-        session['usuario_logado'] = request.form['usuario']
-        flash(request.form['usuario'] + ' logou com sucesso!')
-        proxima_pagina = request.form['proxima']
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if usuario.senha == request.form['senha']:
+            session['usuario_logado'] = usuario.id 
+            flash(usuario.nome + ' logou com sucesso!')
+            proxima_pagina = request.form['proxima']
         return redirect(proxima_pagina)
     else:
         flash('Nao logado, tente novamente!')
